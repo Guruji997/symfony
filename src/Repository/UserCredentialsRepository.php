dbfe84d8e9a4af89d8d228dde68d20a498336aea
Guru Prasad <gpmohanty1080@gmail.com>
2019-09-10 12:25:11 +0530

    commit

---
+++ src/Repository/UserCredentialsRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\UserCredentials;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+/**
+ * @method UserCredentials|null find($id, $lockMode = null, $lockVersion = null)
+ * @method UserCredentials|null findOneBy(array $criteria, array $orderBy = null)
+ * @method UserCredentials[]    findAll()
+ * @method UserCredentials[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class UserCredentialsRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, UserCredentials::class);
+    }
+
+    // /**
+    //  * @return UserCredentials[] Returns an array of UserCredentials objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('u')
+            ->andWhere('u.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('u.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?UserCredentials
+    {
+        return $this->createQueryBuilder('u')
+            ->andWhere('u.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
