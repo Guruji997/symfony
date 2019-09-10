46aa3a9c8e47544a1a28f644d4d35284d6f9b7ea
Guru Prasad <gpmohanty1080@gmail.com>
2019-09-10 12:24:54 +0530

    commit

---
+++ src/Controller/NewUserController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\UserCredentials;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NewUserController extends AbstractController
+{
+    /**
+     * @Route("/new/user", name="new_user")
+     */
+    public function createUser(): Response
+    {
+        // you can fetch the EntityManager via $this->getDoctrine()
+        // or you can add an argument to the action: createProduct(EntityManagerInterface $entityManager)
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $user = new UserCredentials();
+        $user->setName('Guru');
+        $user->setEmail('gpmohanty@gmail.com');
+        $user->setPassword('isha@123');
+
+        // tell Doctrine you want to (eventually) save the User (no queries yet)
+        $entityManager->persist($user);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('New user created with id '.$user->getId());
+    }
+
+    public function index()
+    {
+        var $fname;
+        $femail,$fpassword;
+
+        $user = new UserCredentials();
+        $user->setName('Guru');
+        $user->setEmail('gpmohanty@gmail.com');
+        $user->setPassword('isha@123');
+        
+        return $this->render('new_user/index.html.twig', [
+            'controller_name' => 'NewUserController',
+            'name' => $fname,
+            'email' => $femail,
+            'password' => $fpassword
+        ]);
+    }
+}
